07f4b72794ec516980c8821630af134248fd2aa3
Max Lielje <max@maxlielje.co>
2014-10-23 22:27:18 +0200

    refactoring

--- rcode.go
+++ rcode.go
@@ -1,67 +1,70 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
-type LogRedirects struct {
-	Transport http.RoundTripper
-}
+type redirectLogger struct{}
 
 func main() {
 	// get URL from cli arguments
 	url := os.Args[1]
 
-	// new http client with custom round tripper
-	client := &http.Client{Transport: LogRedirects{}}
+	// new http client with custom round tripper which
+	// itself uses the default transport policy
+	client := &http.Client{
+		Transport:     redirectLogger{},
+		CheckRedirect: redirectCounter}
 
-	// prepare request
-	req, err := http.NewRequest(
-		"GET",
-		url,
-		nil,
-	)
+	// create request
+	req, _ := http.NewRequest("GET", url, nil)
 
 	// … with custom user agent
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36")
-	resp, err := client.Do(req)
 
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fmt.Printf("[%d] %s\n", resp.StatusCode, resp.Request.URL)
+	printResponse(resp)
 }
 
-// RoundTrip only receives a request and returns the response without modifying it
-// Basically we’re doing a regular request and if the code status code is 3xx then we print it out
-func (l LogRedirects) RoundTrip(req *http.Request) (resp *http.Response, err error) {
-	t := l.Transport
-
-	if t == nil {
-		t = http.DefaultTransport
-	}
+// RoundTrip only receives a request, executes it and returns the response
+// without modifications. If the response code is a redirect of some form
+// it gets send down the responses channel to be logged
+func (l redirectLogger) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	t := http.DefaultTransport
 
 	resp, err = t.RoundTrip(req)
 	if err != nil {
 		return
 	}
 
+	// If the response code is 3xx we print it out
 	switch resp.StatusCode {
 	case http.StatusMovedPermanently,
 		http.StatusFound,
 		http.StatusSeeOther,
 		http.StatusTemporaryRedirect:
 
-		// other way of displaying it
-		// newLoc, _ := resp.Location()
-		// fmt.Printf("[%d] %s\n", resp.StatusCode, newLoc.String())
-
-		fmt.Printf("[%d] %s\n", resp.StatusCode, req.URL)
+		printResponse(resp)
 	}
-
 	return
+}
+
+// Increases the max redirect count to 20
+var redirectCounter = func(req *http.Request, via []*http.Request) error {
+	if len(via) >= 20 {
+		return errors.New("Stopped after 20 redirects")
+	}
+	return nil
+}
+
+func printResponse(resp *http.Response) {
+	fmt.Printf("[%d] %s\n", resp.StatusCode, resp.Request.URL)
 }
